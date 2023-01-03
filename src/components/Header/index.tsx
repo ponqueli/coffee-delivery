81b6ce86391047fecd1f30c783ed924d94b5ee53
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-01-02 21:56:23 -0300

    add checkout route

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,18 +2,23 @@ import { MapPin } from 'phosphor-react'
 import { ContentLeft, HeaderContainer, MainContent } from './styles'
 import coffeeLogo from '../../assets/Logo.svg'
 import { ButtonCart } from '../ButtonCart'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <MainContent>
-        <img src={coffeeLogo} alt="Logo of Coffe Delivery app" />
+        <NavLink to="/" title="home">
+          <img src={coffeeLogo} alt="Logo of Coffe Delivery app" />
+        </NavLink>
         <ContentLeft>
           <div>
             <MapPin size={20} weight="fill" />
             <span>Ribeirão Preto, SP</span>
           </div>
-          <ButtonCart amount={10} variant="yellow" onHandleClick={() => {}} />
+          <NavLink to="/checkout" title="checkout">
+            <ButtonCart amount={10} variant="yellow" onHandleClick={() => {}} />
+          </NavLink>
         </ContentLeft>
       </MainContent>
     </HeaderContainer>
